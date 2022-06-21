ad435cda9df94d9ec6999708821e61d9c2fe354d
Steven Chia <stevenchia56@gmail.com>
2022-06-21 22:36:18 +0800

    fix css

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,10 +2,10 @@ import './input.css';
 
 const Input = () => {
     return (
-        <section className='mt-8 flex flex-col'>
+        <section className='input-container'>
             <textarea
                 type='text'
-                className='flex flex-grow w-3/4 h-5/6 p-4 mx-auto my-auto rounded-md bg-white/10 text-2xl font-light tracking-wide text-white placeholder-white/60 focus:outline-none focus:bg-white/20 transition resize-none scrollbar-thin scrollbar-thumb-slate-300'
+                className='block w-3/4 h-full mt-8 p-4 mx-auto my-auto rounded-md bg-white/10 text-base md:text-xl lg:text-2xl font-light tracking-wide text-white placeholder-white/60 focus:outline-none focus:bg-white/20 transition resize-none scrollbar-thin scrollbar-thumb-slate-300'
                 placeholder="What's on your mind?"
             />
         </section>
